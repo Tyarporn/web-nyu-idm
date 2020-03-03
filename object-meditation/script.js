e000e52608726a68263314f23bb92e11e347a8ad
Ty Arpornsuksant <tyarpornsuksant@Tys-MacBook-Pro.local>
2020-03-03 13:04:19 -0500

    played with php and server stuff but am going to work more with cookies later. also added easter egg page

--- object-meditation/script.js
+++ object-meditation/script.js
@@ -1,1 +1,6 @@
 console.log("I'm here, I'm here!!")
+function getPlace() {
+  var x = document.getElementById("places").value;
+  document.getElementById("placeName").innerHTML = x;
+  console.log(x); 
+}
