98af8b2e5c2407c9c88ba9ca8e2db90505ef66eb
Ty Arpornsuksant <tyarpornsuksant@10-18-133-80.dynapool.wireless.nyu.edu>
2020-03-03 16:20:21 -0500

    added some weather stuff and plans to slim down code

--- object-meditation/script.js
+++ object-meditation/script.js
@@ -1,6 +1,18 @@
-console.log("I'm here, I'm here!!")
+let weatherApi= "api.openweathermap.org/data/2.5/weather?q=""{city name},{state};
+let cityName = "New York";
+let stateName = "New York";
+let apiKey = "39031965238faf9cf2930953024067c4";
+let url = weatherApi + cityName + "," + stateName+ "&appid=" + apiKey
+
+
+
+
 function getPlace() {
   var x = document.getElementById("places").value;
   document.getElementById("placeName").innerHTML = x;
-  console.log(x); 
+  console.log(x);
+
+  function getWeather() {
+    
+  }
 }
